f85d59689626ad35ef179ee3d2422f39135753e7
zihyeok <zihyeok@naver.com>
2023-10-30 21:42:36 +0900

    first commit

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,31 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join(){
+        //given ~주어졌을때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when ~이렇게 했을때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then ~이렇게 된다
+        Assertions.assertThat(member).isEqualTo(findMember);
+
+    }
+}
